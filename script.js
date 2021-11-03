88de41cc15258fcd279ffdb53698ffd43b708bea
Artem <lyubavin.web@gmail.com>
2021-11-03 16:16:09 +0300

    Add timer script

---
+++ script.js
@@ -0,0 +1,55 @@
+window.addEventListener('DOMContentLoaded', () => {
+    function setZero(num) {
+        if (num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function getTimeRemaining(deadline) {
+        const total = Date.parse(deadline) - Date.parse(new Date()),
+            days = Math.floor(total / 1000 / 60 / 60 / 24),
+            hours = Math.floor((total / 1000 / 60 / 60) % 24),
+            minutes = Math.floor((total / 1000 / 60) % 60),
+            seconds = Math.floor((total / 1000) % 60);
+
+        return {
+            'total': total,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function displayTimeRemaining(timerSelector, deadline) {
+        const timer = document.querySelector(timerSelector);
+        let days = timer.querySelector('[data-unit="day"] span'),
+            hours = timer.querySelector('[data-unit="hour"] span'),
+            minutes = timer.querySelector('[data-unit="minute"] span'),
+            seconds = timer.querySelector('[data-unit="second"] span'),
+            timerInterval = setInterval(updateTimer, 1000);
+
+        updateTimer();
+
+        function updateTimer() {
+            let time = getTimeRemaining(deadline);
+
+            days.innerText = setZero(time.days);
+            hours.innerText = setZero(time.hours);
+            minutes.innerText = setZero(time.minutes);
+            seconds.innerText = setZero(time.seconds);
+
+            if (time.total <= 0) {
+                clearInterval(timerInterval);
+            }
+        }
+    }
+
+    const deadline = '2022-01-01';
+
+    displayTimeRemaining('.timer', deadline);
+    console.log(Date.parse(deadline));
+    console.log(new Date());
+});
